3a12dce1bcad966337fdc96a76e8bc33c4a86fcd
Pranjal1302 <75469259+Pranjal1302@users.noreply.github.com>
2022-08-25 18:09:20 +0530

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React ,{ useEffect } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {FiSettings } from 'react-icons/fi';
 import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 
 
 function App() {
+  const activeMenu = false;
   return (
     <div>
       <Router>
@@ -13,11 +14,44 @@ function App() {
           <div className = "fixed right-4 bottom-4" style={{ zIndex : '1000'}}>
             <TooltipComponent content="Settings" target="settings" position="Top">
               <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{background:'blue', borderRadius:'50%'}}>
-
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              sidebar
+            </div>
+          ):(
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              sidebar
+            </div>
+          )
+          }
+          <div className={
+            activeMenu ? 'dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full' : 'dark:bg-main-bg bg-main-bg min-h-screen flex-2 w-full'
+          }>
+             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+              navbar
+              </div>
+            </div>
+            <div>
+              <Routes>
+                <Route path= "/" element= "Ecommerce" />
+                <Route path= "/ecommerce" element= "Ecommerce" />
+                <Route path= "/orders" element= "Orders" />
+                <Route path= "/employees" element= "Employees" />
+                <Route path= "/customers" element= "Customers" />
+                <Route path= "/kanbar" element= "Kanbar" />
+                <Route path= "/editor" element= "Editor" />
+                <Route path= "/calendar" element= "Calendar" />
+                <Route path= "/line" element= "Line" />
+                <Route path= "/area" element= "Area" />
+                <Route path= "/bar" element= "Bar" />
+                <Route path= "/pie" element= "Pie" />
+                <Route path= "/pie" element= "Pie" />
+              </Routes>
+            </div>
           </div>
       </Router>
     </div>
---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export {default as Footer} from './Footer'
+export {default as Button} from './Button'
+export {default as Cart} from './Cart'
+export {default as Chat} from './Chat'
+export {default as Header} from './Header'
+export {default as Navbar} from './Navbar'
+export {default as LineChart} from './LineChart'
+export {default as Sidebar} from './Sidebar'
